3169f0cd121a5b4b2fce9e3ca5274a004d85c5a2
Deds ID <81201449+DedsID@users.noreply.github.com>
2023-07-14 09:42:30 +0000

    Update App.js and MainVid.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div className="bg-main text-white h-screen">
       <Header />
       <Navbar />
-      <div className="pl-[10%]">
+      <div className="sm:pl-24">
         {/* <SmallVid /> */}
         <MainVid />
       </div>
--- src/components/MainVid.js
+++ src/components/MainVid.js
@@ -5,39 +5,54 @@ import { faThumbsUp } from "@fortawesome/free-regular-svg-icons";
 
 const MainVid = () => {
   return (
-    <div className="text-text w-2/3">
-      <div className="aspect-video bg-blue-400 rounded-xl">
-        <Video />
-      </div>
+    <div className="text-text md:w-2/3">
+      <div className="max-sm:px-10">
+        <div className="aspect-video bg-blue-400 rounded-xl">
+          <Video />
+        </div>
 
-      {/* judul */}
-      <div className="flex flex-row justify-between py-5">
-        <span className="text-white font-bold text-xl">
-          1000 miles of Sebring stream with Golden Mike - 2022
-        </span>
+        {/* judul */}
+        <div className="flex flex-row justify-between py-5">
+          <span className="text-white font-bold text-xl">
+            1000 miles of Sebring stream with Golden Mike - 2022
+          </span>
+          <div className="flex flex-row gap-5">
+            <FontAwesomeIcon
+              icon={faThumbsUp}
+              className="bg-slate-700 rounded-full p-2 text-white text-xl"
+            />
+            <FontAwesomeIcon
+              icon={faThumbsUp}
+              className="bg-slate-700 rounded-full p-2 text-white text-xl"
+              rotation="180"
+            />
+          </div>
+        </div>
+        {/* end judul */}
+
+        {/* user */}
         <div className="flex flex-row gap-5">
-          <FontAwesomeIcon
-            icon={faThumbsUp}
-            className="bg-slate-700 rounded-full p-2 text-white text-xl"
-          />
-          <FontAwesomeIcon
-            icon={faThumbsUp}
-            className="bg-slate-700 rounded-full p-2 text-white text-xl"
-            rotation="180"
-          />
+          <div className="bg-blue w-10 h-10 rounded-lg"></div>
+          <div className="flex flex-col">
+            <div className="text-white text-lg">
+              Mike Fisher
+              <span className="text-xs border border-1 ml-5 px-7 py-1 rounded-md">
+                Subcribe
+              </span>
+            </div>
+            <div className="text-sm">5 158 223 subcribers</div>
+          </div>
         </div>
-      </div>
-      {/* end judul */}
-
-      {/* user */}
-      <div className="flex flex-row gap-5">
-        <div className="bg-blue w-10 h-10 rounded-lg"></div>
+        {/* end user */}
+        {/* Comment Section */}
         <div>
-            <span>Mike Fisher</span>
-            <div></div>
+          <div className="flex justify-between items-center text-white pt-10">
+            <span className=" font-semibold text-lg"> 7 214 comments </span>
+            <span className="text-sm">Newest First</span>
+          </div>
         </div>
+        {/* End Comment */}
       </div>
-      {/* end user */}
     </div>
   );
 };
